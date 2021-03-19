1a4c3c917cc66cafadbf3c3b0b5d5c820998245d
Marvin Müller <marvin.mueller@code.berlin>
2021-03-19 20:50:52 +0100

    Add user model and implement user route

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 const mongoose = require("mongoose");
 
+const User = require("../models/User");
+
 /* GET users listing. */
-router.get('/', async (req, res) => {
+router.get('/:userId', async (req, res) => {
+    try {
+        let userData = await User.findById(new mongoose.Types.ObjectId(req.params.userId)); 
+        res.send(userData);
+    } catch (error) {
+        res.sendStatus(404);
+    }
 });
 
 module.exports = router;
